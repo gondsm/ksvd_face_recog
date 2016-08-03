c6be7a68cd4052cfeffb3679753b8dfa47d2cc3f
Gonçalo S. Martins <gondsm@gmail.com>
2016-08-03 13:45:58 +0100

    Clean up and re-organize MATLAB code. Add instructions.

--- classify_sequences.m
+++ classify_sequences.m
@@ -1,8 +1,15 @@
-%%
 % Classify Sequences
 % Author: Gonçalo S. Martins
 % This script takes a number of pre-clustered image sequences and builds
 % the appropriate dictionaries, using the K-SVD toolbox <link goes here>.
+%
+% USAGE:
+% Code sections are numbered for user convenience.
+
+%% 1. Decompose Training Data
+% For now, this needs to stay; we're not cleaning up properly at the end
+clear all
+
 % This script assumes the following folder structure:
 %
 % <sequences_root>/seq_<something>/1
@@ -11,9 +18,6 @@
 %                 One per seq.    ^
 %                                 | One per cluster (names should be 1, 2,
 %                                                    etc.)
-
-% For now, this needs to stay; we're not cleaning up properly at the end
-clear all
 
 % Load image clusters
 % For now, filenames are hard-coded (and the above structure is not
@@ -46,7 +50,7 @@ seqs(1).name;
 length(seqs);
 
 % For each subfolder of the main data folder (sequence)
-n = 1 % linear counter for clusters
+n = 1; % linear counter for clusters
 for i = 1:length(seqs)
     % Determine the number of clusters
     n_clusters = dir(strcat(base_dir, seqs(i).name));
@@ -66,26 +70,9 @@ for i = 1:length(seqs)
     end 
 end
 
-% Load test clusters
-test_files{1} = ['sequences_cropped/test/filipa_test/1/1.jpg';
-                 'sequences_cropped/test/filipa_test/1/3.jpg';
-                 'sequences_cropped/test/filipa_test/1/4.jpg';
-                 'sequences_cropped/test/filipa_test/1/6.jpg'];
-              
-for i = 1:length(test_files)
-    % Load all cluster images, cluster by cluster
-    n_imgs = size(test_files{i});
-    n_imgs = n_imgs(1);
-    for j = 1:n_imgs
-        test_clusters{i}{j} = rgb2gray(imread(test_files{i}(i,:)));
-    end
-end
-
 % From here on out, the cell array train_clusters will contain all of the
 % training clusters. Each element of train_clusters is a cell array
 % containing all of the images of that cluster, one per cell.
-% Similarly, the cell array train_clusters will contain all of the test
-% clusters.
 
 % Organize clusters into matrices
 % Each column of the matrix will be a different image
@@ -97,15 +84,6 @@ for i = 1:length(train_clusters)
     end
 end
 %size(train_mats)
-
-test_mats = {};
-for i = 1:length(test_clusters)
-    test_mats{i} = [];
-    for j = 1:length(test_clusters{i})
-        test_mats{i} = [test_mats{i}, double(test_clusters{i}{j}(:))];
-    end
-end
-%size(test_mats)
 
 % Decompose training matrices
 % Whoa, almost looks like a civilized dictionary:
@@ -121,11 +99,67 @@ params=struct('K', 4, ...
 dicts = {};
 decomps = {};
 for i = 1:length(train_mats)
-    info = sprintf('Obtaining dictionary %d of %d.', i, length(train_mats))
+    info = sprintf('Obtaining dictionary %d of %d.', i, length(train_mats));
+    disp(info)
     [dicts{i}, temp] = KSVD(train_mats{i}, params);
     decomps{i} = temp.CoefMatrix;
 end
+disp('Training done!')
 
+% Clean up unneded variables
+clearvars -except dicts
+
+%% 2. Select Test cluster
+% Run ONE of the following sections to select which test cluster will be
+% decomposed
+
+%% Select Cluster 1
+% Load test clusters
+test_files{1} = ['sequences_cropped/test/filipa_test/1/1.jpg';
+                 'sequences_cropped/test/filipa_test/1/3.jpg';
+                 'sequences_cropped/test/filipa_test/1/4.jpg';
+                 'sequences_cropped/test/filipa_test/1/6.jpg'];
+
+
+
+%% Select Cluster 2
+% Load test clusters
+test_files{1} = ['sequences_cropped/test/filipa_test/2/64.jpg';
+                 'sequences_cropped/test/filipa_test/2/65.jpg';
+                 'sequences_cropped/test/filipa_test/2/66.jpg';
+                 'sequences_cropped/test/filipa_test/2/67.jpg';
+                 'sequences_cropped/test/filipa_test/2/68.jpg'];
+
+
+%% 3. Load Test Cluster
+% This section loads the test cluster from the pre-defined image names.
+% the cell array train_clusters will contain all of the test clusters.              
+for i = 1:length(test_files)
+    % Load all cluster images, cluster by cluster
+    n_imgs = size(test_files{i});
+    n_imgs = n_imgs(1);
+    for j = 1:n_imgs
+        test_clusters{i}{j} = rgb2gray(imread(test_files{i}(i,:)));
+    end
+end
+
+% build test matrices
+test_mats = {};
+for i = 1:length(test_clusters)
+    test_mats{i} = [];
+    for j = 1:length(test_clusters{i})
+        test_mats{i} = [test_mats{i}, double(test_clusters{i}{j}(:))];
+    end
+end
+%size(test_mats)
+
+% Clear unnecessary variables
+clearvars -except test_mats dicts
+
+%% 4. Classify Test Cluster
+% This section decomposes the test cluster into each of the dictionaries,
+% and prints the residuals for each dictionary, as well as the dictionary
+% that produced the least error.
 % Decompose test matrices
 disp('Decomposing test sequences into previous dictionaries')
 decomp_results = {};
@@ -153,11 +187,13 @@ for i = 1:length(dicts)
     error(i) = abs(sum(sum(dicts{i}*decomp_results{i} - test_mats{1})));
 end
 disp('Calculated the following residuals:')
-error
+disp(error)
 disp('The training sequence that better represents the test sequence is')
 [temp, idx] = min(error);
-idx
-
+disp(idx)
 
 % Announce classification
 disp('Done!')
+
+% Clear unnecessary variables
+clearvars -except dicts
