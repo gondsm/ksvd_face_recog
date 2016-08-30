d68b8ce5165efdd340555087458bc4498d31a669
Gonçalo S. Martins <gondsm@gmail.com>
2016-08-30 18:34:42 +0100

    MATLAB: add a new scaling feature and the identifiers to all of the clusters.
    The results are now much, much easier to interpret. Also add references
    to new test data.

--- classify_sequences.m
+++ classify_sequences.m
@@ -8,7 +8,7 @@
 
 %% 1. Decompose Training Data
 % Clear any previous training
-clear dicts
+clear dicts train_cluster_names test_cluster_names scale classification_results
 
 % Load train clusters
 % This script assumes the following folder structure:
@@ -23,6 +23,9 @@ base_dir = 'sequences_cropped/train/' % base_dir must end in '/'
 % Extract the folder names of the sequences
 seqs = dir(base_dir);
 seqs = seqs(3:end);
+
+% Scale to resize the images we read
+scale = 0.1;
 
 % For each subfolder of the main data folder (sequence)
 n = 1; % linear counter for clusters
@@ -39,11 +42,15 @@ for i = 1:length(seqs)
         % For each image in the cluster
         for k = 1:length(files)
             filename = strcat(folder, '/', files(k).name);
-            train_clusters{n}{k} = rgb2gray(imread(filename));
+            train_clusters{n}{k} = imresize(rgb2gray(imread(filename)), scale);
         end
+        % Write down the name of the training sequence
+        train_cluster_names{n} = seqs(i).name;
+        % Increment cluster counter
         n = n + 1;
     end 
 end
+fprintf('Loaded %d clusters from directory %s.', n-1, base_dir)
 
 % From here on out, the cell array train_clusters will contain all of the
 % training clusters. Each element of train_clusters is a cell array
@@ -82,7 +89,7 @@ end
 disp('Training done!')
 
 % Clean up unneded variables
-clearvars -except dicts
+clearvars -except dicts train_cluster_names scale
 
 %% 2. Load Test Cluster Filenames
 % This section defines the filenames of the clusters to be used for
@@ -93,7 +100,7 @@ test_files{1} = ['sequences_cropped/test/filipa_test/1/1.jpg';
                  'sequences_cropped/test/filipa_test/1/3.jpg';
                  'sequences_cropped/test/filipa_test/1/4.jpg';
                  'sequences_cropped/test/filipa_test/1/6.jpg'];
-disp('Loaded cluster 1!')
+test_cluster_names{1} = ('filipa');
 
 % Cluster 2
 test_files{2} = ['sequences_cropped/test/filipa_test/2/64.jpg';
@@ -101,7 +108,7 @@ test_files{2} = ['sequences_cropped/test/filipa_test/2/64.jpg';
                  'sequences_cropped/test/filipa_test/2/66.jpg';
                  'sequences_cropped/test/filipa_test/2/67.jpg';
                  'sequences_cropped/test/filipa_test/2/68.jpg'];
-disp('Loaded cluster 2!')
+test_cluster_names{2} = ('filipa');
 
 % Cluster 3
 test_files{3} = ['sequences_cropped/test/filipa_test/3/171.jpg';
@@ -111,7 +118,7 @@ test_files{3} = ['sequences_cropped/test/filipa_test/3/171.jpg';
                  'sequences_cropped/test/filipa_test/3/175.jpg';
                  'sequences_cropped/test/filipa_test/3/176.jpg';
                  'sequences_cropped/test/filipa_test/3/177.jpg'];
-disp('Loaded cluster 3!')
+test_cluster_names{3} = ('filipa');
              
 % Cluster 4
 test_files{4} = ['sequences_cropped/test/goncalo_test/1/22.jpg';
@@ -119,14 +126,14 @@ test_files{4} = ['sequences_cropped/test/goncalo_test/1/22.jpg';
                  'sequences_cropped/test/goncalo_test/1/24.jpg';
                  'sequences_cropped/test/goncalo_test/1/25.jpg';
                  'sequences_cropped/test/goncalo_test/1/26.jpg'];
-disp('Loaded cluster 4!')
+test_cluster_names{4} = ('goncalo');
 
 % Cluster 5
 test_files{5} = ['sequences_cropped/test/goncalo_test/2/03.jpg';
                  'sequences_cropped/test/goncalo_test/2/13.jpg';
                  'sequences_cropped/test/goncalo_test/2/14.jpg';
                  'sequences_cropped/test/goncalo_test/2/15.jpg'];
-disp('Loaded cluster 5!')
+test_cluster_names{5} = ('goncalo');
 
 % Cluster 6
 test_files{6} = ['sequences_cropped/test/goncalo_test/3/102.jpg';
@@ -135,9 +142,52 @@ test_files{6} = ['sequences_cropped/test/goncalo_test/3/102.jpg';
                  'sequences_cropped/test/goncalo_test/3/132.jpg';
                  'sequences_cropped/test/goncalo_test/3/133.jpg';
                  'sequences_cropped/test/goncalo_test/3/134.jpg'];
-disp('Loaded cluster 6!')            
+test_cluster_names{6} = ('goncalo');
 
+% Cluster 7
+test_files{7} = ['sequences_cropped/test/awais_test/1/361.jpg';
+                 'sequences_cropped/test/awais_test/1/362.jpg';
+                 'sequences_cropped/test/awais_test/1/363.jpg';
+                 'sequences_cropped/test/awais_test/1/364.jpg'];
+test_cluster_names{7} = ('awais');
+
+% Cluster 8
+test_files{8} = ['sequences_cropped/test/awais_test/2/16.jpg';
+                 'sequences_cropped/test/awais_test/2/17.jpg';
+                 'sequences_cropped/test/awais_test/2/18.jpg';
+                 'sequences_cropped/test/awais_test/2/19.jpg'];
+test_cluster_names{8} = ('awais');
+
+% Cluster 9
+test_files{9} = ['sequences_cropped/test/awais_test/3/286.jpg';
+                 'sequences_cropped/test/awais_test/3/287.jpg';
+                 'sequences_cropped/test/awais_test/3/288.jpg';
+                 'sequences_cropped/test/awais_test/3/289.jpg'];
+test_cluster_names{9} = ('awais');
              
+% Cluster 10
+test_files{10} = ['sequences_cropped/test/miguel_test/1/431.jpg';
+                  'sequences_cropped/test/miguel_test/1/432.jpg';
+                  'sequences_cropped/test/miguel_test/1/433.jpg';
+                  'sequences_cropped/test/miguel_test/1/434.jpg'];
+test_cluster_names{10} = ('miguel');
+
+% Cluster 11
+test_files{11} = ['sequences_cropped/test/miguel_test/2/316.jpg';
+                  'sequences_cropped/test/miguel_test/2/317.jpg';
+                  'sequences_cropped/test/miguel_test/2/318.jpg';
+                  'sequences_cropped/test/miguel_test/2/319.jpg'];
+test_cluster_names{11} = ('miguel');
+
+% Cluster 12
+test_files{12} = ['sequences_cropped/test/miguel_test/3/152.jpg';
+                  'sequences_cropped/test/miguel_test/3/153.jpg';
+                  'sequences_cropped/test/miguel_test/3/154.jpg';
+                  'sequences_cropped/test/miguel_test/3/155.jpg'];
+test_cluster_names{12} = ('miguel');
+
+
+
 %% 3. Load Test Clusters
 % This section loads the test cluster from the pre-defined image names.
 % the cell array train_clusters will contain all of the test clusters.              
@@ -147,7 +197,7 @@ for i = 1:length(test_files)
     n_imgs = n_imgs(1);
     for j = 1:n_imgs
         %fprintf('Loading file %s into cluster %d.\n', test_files{i}(j,:), i) 
-        test_clusters{i}{j} = rgb2gray(imread(test_files{i}(j,:)));
+        test_clusters{i}{j} = imresize(rgb2gray(imread(test_files{i}(j,:))), scale);
     end
 end
 
@@ -162,9 +212,9 @@ end
 %size(test_mats)
 
 % Clear unnecessary variables
-clearvars -except test_mats dicts
+clearvars -except test_mats dicts train_cluster_names test_cluster_names scale
 
-%% 4. Classify Test Cluster
+%% 4. Classify Test Clusters
 % This section decomposes the test cluster into each of the dictionaries,
 % and prints the residuals for each dictionary, as well as the dictionary
 % that produced the least error.
@@ -172,7 +222,7 @@ clearvars -except test_mats dicts
 disp('Decomposing test sequences into previous dictionaries')
 classification_results = [];
 for n = 1:length(test_mats)
-    fprintf('Decomposing test sequence with dictionary ')
+    fprintf('Decomposing test sequence %d with dictionary ', n)
     decomp_results = {};
     for i = 1:length(dicts)
         fprintf('%d ', i)
@@ -199,19 +249,25 @@ for n = 1:length(test_mats)
     end
     %disp('Calculated the following residuals:')
     %disp(error)
-    disp('The training sequence that better represents the test sequence is')
+    %disp('The training sequence that better represents the test sequence is')
     [temp, idx] = min(error);
-    disp(idx)
+    %disp(idx)
     
-    classification_results = [classification_results; n, idx];
+    
+    %classification_results = [classification_results; n, idx, train_cluster_names(n)];
+    classification_results{n}.matched_cluster = idx;
+    classification_results{n}.test_cluster_name = test_cluster_names(n);
+    classification_results{n}.matched_cluster_name = train_cluster_names(idx);
 end
 
 % Announce classification
-disp('Results: (cluster, match)')
-disp(classification_results)
+disp('Results:')
+for n = 1:length(test_mats)
+    disp(classification_results{n})
+end
 
 % Clear unnecessary variables
-clearvars -except dicts
+clearvars -except dicts train_cluster_names test_cluster_names classification_results scale
 
 %% Clear everything except training
-clearvars -except dicts
+clearvars -except dicts train_cluster_names test_cluster_names
